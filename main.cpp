c765ffeb5fa15a4a09ddd456258689dfd129671a
Ivan Savelyev <s.i.v.892@gmail.com>
2020-02-12 01:56:50 +0300

    curlpp get to value

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <curl/curl.h>
 
 #include <curlpp/Easy.hpp>
+#include <curlpp/Exception.hpp>
+#include <curlpp/Infos.hpp>
 #include <curlpp/Options.hpp>
 #include <curlpp/cURLpp.hpp>
 #include <iostream>
-//#include <sstream>
+#include <sstream>
 
 using namespace curlpp::options;
 int main(int argc, char *argv[]) {
@@ -15,7 +17,14 @@ int main(int argc, char *argv[]) {
     curlpp::Easy myRequest;
     myRequest.setOpt<Url>(myUrl);
     myRequest.setOpt(new Verbose(true));
+
+    std::ostringstream response;
+    myRequest.setOpt(new curlpp::options::WriteStream(&response));
     myRequest.perform();
+    std::cout << "\n\n\nRESULT\n\n\n";
+    std::cout << "Begin response: \n[\n"
+              << response.str() << "\n]\nEnd response... ";
+
   } catch (curlpp::RuntimeError &e) {
     std::cout << e.what() << std::endl;
   } catch (curlpp::LogicError &e) {
